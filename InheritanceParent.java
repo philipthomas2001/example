fcf1d827ce2538e495fed3ff5c7c95945974003a
philipthomas2001 <philiptchorath2001@gmail.com>
2024-03-11 12:25:14 -0500

    changes

---
+++ InheritanceParent.java
@@ -0,0 +1,14 @@
+public class InheritanceParent {
+
+
+    public int add(int number,int number2){
+        return number+number2;
+    }
+    //overloading
+    protected int add(int number,int number2,int number3){
+        return number+number2+number3;
+    }
+    int sub(int number,int number2){
+        return number-number2;
+    }
+}
